8d1c5ae23581d19210c4da376bec7868579a13ad
alijan1978 <mohammadalinaim32@gmail.com>
2023-11-22 10:13:44 -0800

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+            Thread.sleep(1000);
+        }
+
+    }
+}
+
+/*
+http://jenkins.syntaxtechs.net:8080/
+UserName:Syntax
+Password:Syntax123
+ */
