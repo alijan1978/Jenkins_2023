37591a268f5e6c12571f90f6a3819c3bca417b66
alijan1978 <mohammadalinaim32@gmail.com>
2023-12-08 12:07:25 -0800

    New note

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i <= 20; i++) {
-            System.out.println("Hello world!");
+            System.out.println("This is the Jenkins class!");
             Thread.sleep(1000);
         }
 
